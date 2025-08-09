692bfdb8aa22112884b419f84d05106ae8b1252c
Blake0v2 <dumboyonpc@outlook.com>
2025-08-08 22:52:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const clientId = '1389852325648007290';
 const clientSecret = 'dWOJvWCWiFWTKiw7xmrQa1iLoY7Pd6Ng';
-const redirectUri = 'http://localhost:8000/api/auth/discord/redirect';
+const redirectUri = 'https://blake0v2.github.io/TheArchAngels/Log%20in.html';
 
 // Serve the static frontend (if you deploy to GitHub Pages)
 app.use(express.static('public'));
@@ -15,7 +15,7 @@ app.get('/oauth2/login', (req, res) => {
     const authUrl = oauth.generateAuthUrl({
         clientId: '1389852325648007290',
         scope: ['identify', 'guilds'],
-        redirectUri: 'http://local:8000/oauth2/callback', // Match this with your Discord redirect URI
+        redirectUri: 'https://blake0v2.github.io/TheArchAngels/Log%20in.html', // Match this with your Discord redirect URI
     });
     res.redirect(authUrl); // This should redirect to Discord's OAuth2 verification page
 });
@@ -51,5 +51,6 @@ app.get('/api/current-raid', (req, res) => {
 });
 
 app.listen(8000, () => {
-    console.log('Server running on http://localhost:8000/api/auth/discord/redirect');
+    console.log('Server running on https://blake0v2.github.io/TheArchAngels/Log%20in.html');
 });
+
